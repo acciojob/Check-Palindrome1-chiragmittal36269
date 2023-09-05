276190f212a43ee6bafe99fe6704891456d73e24
Chirag Mittal <118048068+chiragmittal36269@users.noreply.github.com>
2023-09-05 20:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 // complete the given function
 
 function palindrome(str){
-	if(str.length == 0){
-		return false;
-	}
-	let i = 0;
-	let j = str.length-1;
-	while(i<=j){
-		if(str.charAt(i) != str.charAt(j)){
-			return false;
-		}
-		else
-		{
-			i++;
-			j--;
-		}
-	}
-	return true;
+	    if (str.length === 0 || str.length === 1) {
+        return true; // An empty string and single characters are considered palindromes.
+    }
+    
+    let i = 0;
+    let j = str.length - 1;
+    
+    while (i <= j) {
+        if (str.charAt(i) !== str.charAt(j)) {
+            return false;
+        } else {
+            i++;
+            j--;
+        }
+    }
+    
+    return true;
 
 }
 module.exports = palindrome
